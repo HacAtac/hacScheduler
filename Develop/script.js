2c649834a808b5517d5a32fbf371a24304bd5068
HacAtac <jhack00@icloud.com>
2021-09-09 09:51:32 -0500

    started local storage with a click function

--- Develop/script.js
+++ Develop/script.js
@@ -8,32 +8,34 @@ function whatTime() {
     //var to hold the current hour using moment.js to retrieve
     //Need moment.js to get current hour moment().hour();
     var currentTime = moment().hours();
+    console.log(currentTime);
     //need to loop over the blocks to figure out and choose if past present future
     //targets the block class in html then loops over the blocks to check hour
     //need to loop over time blocks and check with conditional if < or = if, else if, if
-    $(".block").each(function(){
+    $(".time-block").each(function(){
         var thisHour = parseInt($(this).attr("id"));
         console.log(thisHour);
 
         if (thisHour > currentTime) {
             $(this).addClass("future");
+           
         }else if (thisHour === currentTime){
             $(this).addClass("present");
+            
         }else {
             $(this).addClass("past");
+            
         }
     })
 };
 whatTime();
-//When I click in time block then i can enter an event
-
-
 //When I click the save button it saves it in local storage
 //Posibly jQuery event listener in a function and then save time/text in local storage
-
-
-
 //When I refresh the page the saved events are still there
+    $(".saveBtn").on("click", function(){
+        var text = $(this).description();
+        console.log(text);
+    })
 
 
 
