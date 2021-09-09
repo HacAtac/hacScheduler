f910c2415d96edd07284882e53e3509552d3cff7
HacAtac <jhack00@icloud.com>
2021-09-08 23:09:13 -0500

    added function with conditions so that it chooses what color depending on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,45 @@
+var hour9 = hour9;
+
 //grabs from moment.js
 var currentDay = moment().format('dddd MMMM do YYYY');
 // calls the current day moment variable
 $("#currentDay").text(currentDay);
 
+
+
+
 //When I view time blocks they need to be color-coded for past,present,future
+function whatTime() {
+    //var to hold the current hour using moment.js to retrieve
+    //Need moment.js to get current hour moment().hour();
+    var currentTime = moment().hours();
+    //need to loop over the blocks to figure out and choose if past present future
+    //targets the block class in html then loops over the blocks to check hour
+    //need to loop over time blocks and check with conditional if < or = if, else if, if
+    $(".block").each(function(){
+        var thisHour = parseInt($(this).attr("id"));
 
-
+        if (thisHour > currentTime) {
+            $(this).addClass("future");
+        }else if (thisHour === currentTime){
+            $(this).addClass("present");
+        }else {
+            $(this).addClass("past");
+        }
+    })
+};
+whatTime();
 //When I click in time block then i can enter an event
 
 
 //When I click the save button it saves it in local storage
 //Posibly jQuery event listener in a function and then save time/text in local storage
-//Need moment.js to get current hour moment().hour();
-//need to loop over time blocks and check with conditional if < or = if, else if, if
+
 
 
 //When I refresh the page the saved events are still there
+
+
+
+
+
