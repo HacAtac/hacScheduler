6342c1da388d54a2f7b2f2f18551b960a1965748
HacAtac <jhack00@icloud.com>
2021-09-08 21:26:50 -0500

    added my time blocks in html with row/col from bootstrap added button with fas fa-save bootstrap added psuedo code into js

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,17 @@
 var currentDay = moment().format('dddd MMMM do YYYY');
 // calls the current day moment variable
 $("#currentDay").text(currentDay);
+
+//When I view time blocks they need to be color-coded for past,present,future
+
+
+//When I click in time block then i can enter an event
+
+
+//When I click the save button it saves it in local storage
+//Posibly jQuery event listener in a function and then save time/text in local storage
+//Need moment.js to get current hour moment().hour();
+//need to loop over time blocks and check with conditional if < or = if, else if, if
+
+
+//When I refresh the page the saved events are still there
